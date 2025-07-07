5f331aa31ae85eb923d00b47172cff3775fd4760
白彩恋 <shiro@oom-wg.dev>
2025-07-07 18:05:55 +0800

    优化代码

--- base/src/main/kotlin/Base.kt
+++ base/src/main/kotlin/Base.kt
@@ -1,22 +1,20 @@
 package dev.oom_wg.wm.wmlang
 
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.LocaleList
-import com.highcapable.betterandroid.system.extension.tool.SystemVersion
 import java.util.*
 
 object WMLangProvider {
-    fun getTags() = mutableListOf<String>().also {
-        if (SystemVersion.isHighOrEqualsTo(SystemVersion.N)) {
-            val list = LocaleList.getDefault()
-            for (i in 0 until list.size()) {
-                val loc = list[i]
-                it += loc.toLanguageTag().replace('-', '_')
-                if (loc.language !in it) it += loc.language
-            }
-        } else {
-            val loc = Locale.getDefault()
-            it += if (loc.country.isNullOrBlank()) loc.language else "${loc.language}_${loc.country}"
-            if (loc.language !in it) it += loc.language
+    fun getTags() = buildList {
+        (if (VERSION.SDK_INT >= VERSION_CODES.N) LocaleList.getDefault().run { List(size()) { this[it] } }
+        else listOf(Locale.getDefault())).forEach { loc ->
+            when {
+                VERSION.SDK_INT >= VERSION_CODES.N -> loc.toLanguageTag().replace('-', '_')
+                loc.country.isNullOrBlank() -> loc.language
+                else -> "${loc.language}_${loc.country}"
+            }.also { add(it) }
+            if (loc.language !in this) add(loc.language)
         }
     }
 
@@ -30,6 +28,6 @@ object WMLangProvider {
     }
 }
 
-class WMLangBase(val dflt: String, val values: Map<String, String>) {
-    override fun toString() = WMLangProvider.locTags.firstNotNullOfOrNull { values[it] } ?: dflt
+class WMLangBase(val dflt: String, val vals: Map<String, String>) {
+    override fun toString() = WMLangProvider.locTags.firstNotNullOfOrNull { vals[it] } ?: dflt
 }
